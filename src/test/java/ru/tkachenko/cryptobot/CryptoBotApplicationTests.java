514fabd63c495091f35071b33fec6e924daacdb2
Rodion Tkachenko <tkachenko_rp@mail.ru>
2024-03-13 14:07:32 +0700

    init

---
+++ src/test/java/ru/tkachenko/cryptobot/CryptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tkachenko.cryptobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
